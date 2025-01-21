8eecad19c6f1d390b59a5fe7068e2c443b09d48a
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2025-01-21 10:53:07 -0400

    somes changes

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,7 +1,21 @@
 /**
  * @license MIT
- * @copyright 2023 codewithsadee
- * @author codewithsadee <mohammadsadee24@gmail.com>
+ * @copyright 2024 ajedev
+ * @author ajedev <arnaldoespinoza1@hotmail.com>
  */
 
 "use strict";
+
+/**
+ * Add event on multiple elements
+ * @param {NodeList} $elements Nodelist 
+ * @param {String} eventType Eent type string
+ * @param {Function} callback Callback function
+ */
+
+
+window.addEventOnElements = ($elements, eventType, callback)=>{
+    for (const $element of $elements) {
+        $element.addEventListener(eventType, callback)
+    }
+}
--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,74 @@
 /**
  * @license MIT
- * @copyright 2023 codewithsadee
- * @author codewithsadee <mohammadsadee24@gmail.com>
+ * @copyright 2024 ajedev
+ * @author ajedev <arnaldoespinoza1@hotmail.com>
  */
 
 "use strict";
+
+// Home page search
+
+const /** {NodeElement} */ $searchField = document.querySelector(
+    "[data-search-field]"
+  );
+const /** {NodeElement} */ $searchBtn =
+    document.querySelector("[data-search-btn]");
+
+$searchBtn.addEventListener("click", function () {
+  if ($searchField.value)
+    window.location = `/recipes.html?q=${$searchField.value}`;
+});
+
+// search submit when press enter key
+
+$searchField.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") $searchBtn.click();
+});
+
+// tab panel navigation
+
+const /** NodeList */ $tabBtns = document.querySelectorAll("[data-tab-btn]");
+const /** NodeList */ $tabPanels =
+    document.querySelectorAll("[data-tab-panel]");
+
+let /** nodeElement */ [$lastActiveTabPanel] = $tabPanels;
+let /** nodeElement */ [$lastActiveTabBtn] = $tabBtns;
+
+addEventOnElements($tabBtns, "click", function () {
+  $lastActiveTabPanel.setAttribute("hidden", "");
+  $lastActiveTabBtn.setAttribute("aria-selected", false);
+  $lastActiveTabBtn.setAttribute("tabindex", -1);
+
+  const /** {NodeElement} */ $currentTabPanel = document.querySelector(
+      `#${this.getAttribute("aria-controls")}`
+    );
+
+  $currentTabPanel.removeAttribute("hidden");
+  this.setAttribute("aria-selected", true);
+  this.setAttribute("tabindex", 0);
+
+  $lastActiveTabPanel = $currentTabPanel;
+  $lastActiveTabBtn = this;
+});
+
+/**
+ * Navigate tab with arrow key
+ */
+
+addEventOnElements($tabBtns, "keydown", function (e) {
+  const /** {NodeElement} */ $nextElement = this.nextElementSibling;
+  const /** {NodeElement} */ $previousElement = this.previousElementSibling;
+
+  if (e.key === "ArrowRight" && $nextElement) {
+    this.setAttribute("tabindex", -1);
+    $nextElement.setAttribute("tabindex", 0);
+    $nextElement.focus();
+  } else if (e.key === "ArrowLeft" && $previousElement) {
+    this.setAttribute("tabindex", -1);
+    $previousElement.setAttribute("tabindex", 0);
+    $previousElement.focus();
+  } else if (e.key === "Tab") {
+    this.setAttribute("tabindex", -1);
+    $lastActiveTabBtn.setAttribute("tabindex", 0);
+  }
+});
